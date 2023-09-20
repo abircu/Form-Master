b2d0523ac1ce05e05bb9a618e2beec1b8d7e6284
abircu <mdabircse4873@gmail.com>
2023-09-21 00:28:09 +0530

    passing to get value of input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import SimpleForm from "./Components/SimpleForm/SimpleForm";
 function App() {
   return (
     <>
-      <h1>hsk</h1>
+      <SimpleForm />
     </>
   );
 }
--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 const SimpleForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+  };
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input className="bg-gray-200" type="text" name="name" />
         <br />
-        <button>Submit</button>
+        <input className="bg-gray-200" type="email" name="email" />
+        <br />
+        <input type="number" name="number" id="" />
+        <br />
+        <input className="bg-black text-white" type="submit" value="Submit" />
       </form>
     </div>
   );
