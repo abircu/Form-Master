d98d909cb5c19a67e2be2d8f6758483dede6ea44
abircu <mdabircse4873@gmail.com>
2023-09-20 23:15:50 +0530

    project setup of form master thit project  handle by react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>hsk</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SimpleForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
